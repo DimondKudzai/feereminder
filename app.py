5ddc5d4b7fea5c5167a1bc26e771109e984b4323
DimondKudzai <diamondkudzai70@gmail.com>
2026-05-06 10:00:25 +0200

    initial add

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, g, jsonify
 import os, json, csv, requests
-import psycopg as psycopg2
+import psycopg2
 import fitz #PyMuPDF
 import openpyxl
 from datetime import datetime
